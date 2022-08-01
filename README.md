3927462bfc9cb0d74c9ac65efc97c5a162c2c451
Bryn Cooke <bryncooke@gmail.com>
2022-08-01 20:49:27 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# apollo-schemas
+# Apollo Schemas
+
+Repository of Apollo json schemas for use with https://www.schemastore.org/.
+
+## Development
+
+Use the following directory layout:
+
+```
+/
+├─ meta-schema.json       // static
+├─ <product>/             
+│  ├─ meta-schmea.json    // generated
+│  ├─ schemas/            
+│  │  ├─ <version>.json
+```
+
+Where:
+* &lt;product&gt; - is the name of the product.
+* &lt;version&gt;.json - is a schema for a version of the product.
+
+Adding new schemas in the appropriate location will cause the meta-schema.json for that project to be updated.
+
---
+++ rebuild-meta-schemas.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const fs = require('fs');
+const directoryPath = path.join(__dirname, '.');
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan root directory: ' + err);
+    }
+
+    files.forEach(function (schema_directory) {
+        if (!schema_directory.startsWith(".") && fs.statSync(schema_directory).isDirectory()) {
+
+            let schemas = []
+
+            const directoryPath = path.join(__dirname, schema_directory, "schemas");
+            fs.readdir(directoryPath, function (err, files) {
+                if (err) {
+                    return console.log('Unable to scan schema directory: ' + err);
+                }
+
+                files.forEach(function (schema_file) {
+
+                    let schema = {
+                        "allOf": [
+                            {
+                                "properties": {
+                                    "version": {
+                                        "const": "v0.12.0"
+                                    }
+                                }
+                            },
+                            {
+                                "$ref": `https://raw.githubusercontent.com/apollographql/apollo-schemas/main/${schema_directory}/schemas/${schema_file}`
+                            }
+                        ]
+                    };
+                    schemas.push(schema);
+                });
+
+                let output = {
+                    "$id": `https://github.com/apollographql/apollo-schemas/tree/main/${schema_directory}/meta-schema.json`,
+                    "$schema": "https://github.com/apollographql/apollo-schemas/tree/main/meta-schema.json",
+                    "title": "All router config versions",
+                    "type": "object",
+                    "oneOf": schemas
+                }
+
+                fs.writeFile(path.join(__dirname, schema_directory, "meta-schema.json"), JSON.stringify(output, null, 2), function (err) {
+                    if (err) {
+                        return console.log('Unable to write meta-schema: ' + err);
+                    }
+                });
+            });
+        }
+    });
+});
